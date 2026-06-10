f32775e2d0eced26cc59fa0c231186319e9e80a6
Arzymat <_keepa3003@gmail.com>
2026-06-10 18:52:17 +0600

    routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,26 @@
-import React from 'react';
+import React, {Suspense} from 'react';
 import {Counter} from "./components/Counter";
-import './index.scss'
+import './styles/index.scss'
+import {Link, Route, Routes} from "react-router-dom";
+import AboutPage from "./pages/AboutUsPage/AboutPage";
+import MainPage from "./pages/MainPage/MainPage";
+import {AboutPageAsync} from "./pages/AboutUsPage/AboutPage.async";
+import {MainPageAsync} from "./pages/MainPage/MainPage.async";
 
 
 const App = () => {
     return (
         <div>
-         <Counter/>
+            <Link to={"/"}>Главная</Link>
+            <Link to={"/about"}>О нас</Link>
+            <Suspense fallback={<div>Loading...</div>}>
+                 <Routes>
+
+                        <Route path={"/about"} element={<AboutPageAsync/>}/>
+                        <Route path={"/"} element={<MainPageAsync/>}/>
+
+                </Routes>
+            </Suspense>
         </div>
     );
 };
